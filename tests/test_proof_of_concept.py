fa459bc22785d09df70de02b6246c24995108191
Irina Tenitska <moore.evelyn13@gmail.com>
2020-12-29 18:07:17 +0200

    test_proof_of_concept redacted

--- tests/test_proof_of_concept.py
+++ tests/test_proof_of_concept.py
@@ -4,23 +4,20 @@ from selene.support.shared import browser
 
 def test_common_todo_functionality():
     browser.open('https://todomvc4tasj.herokuapp.com/')
-
     browser.should(have.js_returned(True, "return $._data($('#clear-completed')[0], 'events').hasOwnProperty('click')"))
 
-    # Create
     browser.element('#new-todo').type('a').press_enter()
     browser.element('#new-todo').type('b').press_enter()
     browser.element('#new-todo').type('c').press_enter()
-
-    # Read
     browser.all('#todo-list>li').should(have.exact_texts('a', 'b', 'c'))
 
-    # Update
     browser.all('#todo-list>li').element_by(have.exact_text('b')).double_click()
     browser.all('#todo-list>li').element_by(have.css_class('editing'))\
         .element('.edit').type(' edited').press_enter()
+
     browser.all('#todo-list>li').element_by(have.exact_text('b edited'))\
         .element('.toggle').click()
+
     browser.element('#clear-completed').click()
     browser.all('#todo-list>li').should(have.exact_texts('a', 'c'))
 
@@ -28,7 +25,6 @@ def test_common_todo_functionality():
     browser.all('#todo-list>li').element_by(have.css_class('editing'))\
         .element('.edit').type(' to be canceled').press_escape()
 
-    # Delete
     browser.all('#todo-list>li').element_by(have.exact_text('c'))\
         .hover().element('.destroy').click()
     browser.all('#todo-list>li').should(have.exact_texts('a'))
